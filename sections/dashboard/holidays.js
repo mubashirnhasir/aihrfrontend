1ce9e477dba961d44cc813a31783192da3e04821
mubashirnhasir <mubashirnhasir@gmail.com>
2025-04-16 01:02:22 +0530

    Fixed minor issue

--- sections/dashboard/holidays.js
+++ sections/dashboard/holidays.js
@@ -11,7 +11,7 @@ const Holidays = () => {
             </div>
             <div className='h-[1px] bg-gray-300 w-full my-2'></div>
             <div className='flex felx-col gap-2'>
-                <div className="card p-4 flex items-start gap-2 bg-gradient-to-b from-[#38D2F5] to-[#0BA5EC] w-full rounded-lg border border-white h-[100px] shadow-[0px_22px_18px_-12px_rgba(0,0,0,0.14)]">
+                <div className="card p-4 flex items-start gap-2 bg-gradient-to-b from-[#38D2F5] to-[#0BA5EC] w-full rounded-lg border border-white h-[100px] cursor-pointer transition-all hover:shadow-[0px_22px_18px_-12px_rgba(0,0,0,0.14)]">
                         <div className='bg-[#0BA5EC] p-2 rounded-lg border border-white'><Calendar color={"white"} /></div>
                         <div className='text-white'>
                             <div className='text-lg font-semibold'>Independance Day</div>
