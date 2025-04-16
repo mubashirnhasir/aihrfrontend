c442d9a0af63d129e9b297b5bf42f7cfae4c96c7
mubashirnhasir <mubashirnhasir@gmail.com>
2025-04-16 23:29:00 +0530

    Add search functionality to Employee Attendance component

--- app/dashboard/attendance/allemployees/page.js
+++ app/dashboard/attendance/allemployees/page.js
@@ -1,12 +1,14 @@
+"use client"
 import EmployeeAttendance from '@/sections/attendance/employeeAttendance'
-import React from 'react'
+
 
 const Page = () => {
-  return (
-    <div>
-         <EmployeeAttendance />
-    </div>
-  )
+    
+    return (
+        <div>
+            <EmployeeAttendance  />
+        </div>
+    )
 }
 
 export default Page
